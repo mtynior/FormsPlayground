443c5e507e3feeb87c6d43ade2119b181e2e237e
Michal Tynior <michal.git@tynior.com>
2022-07-14 17:11:30 +0200

    Initial Commit

---
+++ FormsPlaygrounds/FormsPlaygroundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormsPlaygroundsApp.swift
+//  FormsPlaygrounds
+//
+//  Created by Michal on 14/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct FormsPlaygroundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
